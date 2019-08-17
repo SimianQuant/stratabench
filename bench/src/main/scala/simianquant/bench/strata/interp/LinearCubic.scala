d014a6272088b8a1c250df45cd41634c80692e2a
Harshad Deo <harshad@simianquant.com>
2019-08-17 10:55:40 +0530

    add BiLinead and LinearCubic interpolation

---
+++ bench/src/main/scala/simianquant/bench/strata/interp/LinearCubic.scala
@@ -0,0 +1,52 @@
+package simianquant.bench.strata.interp
+
+import java.util.concurrent.TimeUnit
+import org.openjdk.jmh.infra.Blackhole
+import org.openjdk.jmh.annotations._
+import com.opengamma.strata.basics.value.ValueDerivatives
+import simianquant.strata.setup.InterpolatorWrapper
+
+object LinearCubic {
+
+  @State(Scope.Benchmark)
+  class StrataInterp {
+    val instance = InterpolatorWrapper.linearCubic(InterpolationData2D.data)
+  }
+
+}
+
+/** Benchmarks Strata's implementation of linear interpolation
+  *
+  * @author Harshad Deo
+  */
+@BenchmarkMode(Array(Mode.SampleTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class LinearCubic {
+
+  import LinearCubic._
+
+  @Benchmark
+  def value(interp: StrataInterp, data: InterpolationData2D.Data, bh: Blackhole): Unit = {
+    var ctr = 0
+    while (ctr < data.xs.length) {
+      val x = data.xs(ctr)
+      val y = data.ys(ctr)
+      bh.consume(interp.instance.interpolate(x, y))
+      ctr += 1
+    }
+  }
+
+  @Benchmark
+  def sensitivity(interp: StrataInterp, data: InterpolationData2D.Data, bh: Blackhole): Unit = {
+    var ctr = 0
+    while (ctr < data.xs.length) {
+      val x = data.xs(ctr)
+      val y = data.xs(ctr)
+      val value = interp.instance.interpolate(x, y)
+      val sens = interp.instance.parameterSensitivity(x, y)
+      bh.consume(ValueDerivatives.of(value, sens))
+      ctr += 1
+    }
+  }
+
+}
--- setup/src/main/scala/simianquant/strata/setup/InterpolatorWrapper.scala
+++ setup/src/main/scala/simianquant/strata/setup/InterpolatorWrapper.scala
@@ -1,7 +1,8 @@
 package simianquant.strata.setup
 
 import com.opengamma.strata.collect.array.DoubleArray
-import com.opengamma.strata.market.curve.interpolator.{BoundCurveInterpolator, CurveExtrapolators, CurveInterpolators}
+import com.opengamma.strata.market.curve.interpolator._
+import com.opengamma.strata.market.surface.interpolator.{BoundSurfaceInterpolator, GridSurfaceInterpolator}
 
 /** Wrapper around Strata's weird syntax to generate interpolators
   *
@@ -19,6 +20,20 @@ object InterpolatorWrapper {
     val interp = CurveInterpolators.NATURAL_CUBIC_SPLINE.bind(xs, ys)
     val extrap = CurveExtrapolators.FLAT.bind(xs, ys, interp)
     interp.bind(extrap, extrap)
+  }
+
+  def biLinear(data: Array[(Double, Double, Double)]): BoundSurfaceInterpolator = {
+    val (xs, ys, zs) = data.unzip3
+    GridSurfaceInterpolator
+      .of(CurveInterpolators.LINEAR, CurveInterpolators.LINEAR)
+      .bind(DoubleArray.ofUnsafe(xs), DoubleArray.ofUnsafe(ys), DoubleArray.ofUnsafe(zs))
+  }
+
+  def linearCubic(data: Array[(Double, Double, Double)]): BoundSurfaceInterpolator = {
+    val (xs, ys, zs) = data.unzip3
+    GridSurfaceInterpolator
+      .of(CurveInterpolators.LINEAR, CurveInterpolators.NATURAL_CUBIC_SPLINE)
+      .bind(DoubleArray.ofUnsafe(xs), DoubleArray.ofUnsafe(ys), DoubleArray.ofUnsafe(zs))
   }
 
 }
